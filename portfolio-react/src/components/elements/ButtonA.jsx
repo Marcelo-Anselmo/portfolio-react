0b3f829d43ba19f288a23d8a1e4b7faa39d8c870
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-09-12 19:11:24 -0300

    Presentation Section added
    Presentation section finished && Added

---
+++ portfolio-react/src/components/elements/ButtonA.jsx
@@ -0,0 +1,15 @@
+import styles from './ButtonA.module.css'
+
+function ButtonA({text, link}) {
+    return (
+      <>
+        <div>
+          <a href={link} target='blank'>
+            <button className={styles.btn}>{text}</button>
+          </a>
+        </div>
+      </>
+    );
+}
+
+export default ButtonA
--- portfolio-react/src/components/section/Presentation.jsx
+++ portfolio-react/src/components/section/Presentation.jsx
@@ -1,18 +1,30 @@
+import styles from './Presentation.module.css'
+import ButtonA from '../elements/ButtonA';
+// import ButtonB from '../elements/ButtonB';
+
 function Presentation() {
   return (
     <>
-      <div id="Presenation">
-        <h1>Essa é minha Presentation</h1>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      <div className={styles.presentation} id="Presenation">
+        <h4>
+          <strong>Bem-vindo ao meu Portfólio</strong>
+        </h4>
+        <h1>Olá, eu sou Marcelo Anselmo!</h1>
+        <p>
+          Sou um apaixonado por tecnologia e soluções inovadoras.Como Product{" "}
+          <br />
+          Manager, eu tenho o compromisso de resolver problemascomplexos e{" "}
+          <br />
+          trazer resultados excepcionais para os negócios.Meus projetos já{" "}
+          <br />
+          geraram milhões de reais em receita anualestou sempre em busca de{" "}
+          <br />
+          novos desafios para superar.
+        </p>
+        <ButtonA
+          link="https://github.com/Marcelo-Anselmo"
+          text="Conecte-se comigo!!"
+        />
       </div>
     </>
   );
